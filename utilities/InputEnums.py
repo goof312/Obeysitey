774479657b72c2d420d4eae74d04aa8fab88243e
Nin <55144487+JonPeterP@users.noreply.github.com>
2024-05-16 12:38:05 +0800

    Update Prediction input
    - update input for Prediction method
    - allow string input for specific attributes (gender, CALC, FAVC, SCC, Smoke, History, CAEC, Trans

---
+++ utilities/InputEnums.py
@@ -0,0 +1,41 @@
+from enum import IntEnum
+
+class Gender(IntEnum):
+    male = 1
+    female = 0
+    
+class CALC(IntEnum):
+    no = 0
+    sometimes = 1
+    frequently = 2
+    always = 3
+
+class YesNo(IntEnum):
+    yes = 1
+    no = 0
+
+class CAEC(IntEnum):
+    no = 0
+    sometimes = 1
+    frequently = 2
+    always = 3
+    
+class MTRANS(IntEnum):
+    automobile = 0
+    motorbike = 1
+    bike = 2
+    public_transportation = 3
+    walking = 4
+
+#throw this
+class FAVC(IntEnum):
+    yes = 1
+    no = 0
+    
+class SCC(IntEnum):
+    yes = 1
+    no = 0
+
+class Smoke(IntEnum):
+    yes = 1
+    no = 0
