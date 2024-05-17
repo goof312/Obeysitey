f74f56e1af03ce21e68bf3498b757b3c780b19b9
jzrll <jetjetllumbera@gmail.com>
2024-05-17 14:52:10 +0800

    add functions in controller

--- controller/MainController.py
+++ controller/MainController.py
@@ -5,6 +5,7 @@ import sys
 # iba iba tayo dito
 sys.path.append("C:/xampp/htdocs/Obeysity/Obeysitey/")
 from utilities import PredictionModel as PM
+from model import MyQueries
 
 class Controller:
     predictionModel = ""
@@ -17,8 +18,26 @@ class Controller:
 
     def predict(self):
         self.predictionModel.PrintStats()
+        # kuha pa ng input sa views
         #SAMPLE PREDICTION AND INPUT
         print(self.predictionModel.Predict([23, "Male", 1.65, 60, "No", "No", 2, 1, "No", "No", 3, "No", 1, 2, "Sometimes", "Public_Transportation"]))
+    
+    # view ------------------------------
+    def get_Input(self):
+        pass
+
+    def return_Prediction(self):
+        # call views na magpapakita ng result
+        pass
+    
+    # model -----------------------------
+    def store(self):
+        # db
+        pass
+    
+    def get(self):
+        # view all inputs
+        pass
 
 cont = Controller()
 cont.predict()
