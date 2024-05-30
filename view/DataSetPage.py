d4ddd369751b4e1b3401f45f5f68d44d8da9acc1
Jon Peter Pomperada <55144487+JonPeterP@users.noreply.github.com>
2024-05-30 08:00:31 +0800

    navbarclass test

--- view/DataSetPage.py
+++ view/DataSetPage.py
@@ -3,10 +3,14 @@
 
 
 import cgi
+import NavBarView as nav
 
 
 class MyDataSetPageView(object):
+    nv = ""
+
     def __init__(self):
+        self.nv = nav()
         return
 
     def viewDataPage(self):
@@ -24,45 +28,12 @@ class MyDataSetPageView(object):
             </head>
 
             <body>
-                <nav class="navbar navbar-expand-lg " style="background-color: #0d6efd;">
-                    <div class="container-fluid">
-                        <a class="navbar-brand text-white" href="#">Obeysitey</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item ">
-                                    <form action="MainController.py" method="post" style="margin-bottom: 0;">
-                                        <input name="back" type="submit" class="nav-link active text-white" aria-current="page"
-                                            value="Home" />
-                                    </form>
-                                </li>
-                                <li class="nav-item">
-                                    <form action="MainController.py" method="post" style="margin-bottom: 0;">
-                                        <input name="dataList" type="submit" class="nav-link active text-white" aria-current="page"
-                                            value="Data" />
-                                    </form>
+            """
+        )
+        print(self.nv.NavBarView())
 
-                                </li>
-                                <li class="nav-item">
-                        <form action="MainController.py" method="post" style="margin-bottom: 0;">
-                            <input name="dataset" type="submit" class="nav-link active text-white" aria-current="page"
-                                value="About" />
-                        </form>
-
-                    </li>
-                            </ul>
-                            <form class="form-inline mt-2 mt-lg-0 d-flex" action="MainController.py" method="post" style="margin-bottom: 0;">
-                                <input type="submit" class="btn text-white" style="background-color: #418cfd;" name="TAKE"
-                                    value="Take The Test" />
-                            </form>
-                        </div>
-                    </div>
-                </nav>
-
+        print(
+            """
                 <div class="container mt-5 mb-5">
 
                 <h1 class="display-5 text-center mt-5">Obesity</h1>
